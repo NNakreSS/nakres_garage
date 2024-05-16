4c92092999db9bebc8684da7a7c3820ad4e672a2
NNakreSS <serkanatmaca15@gmail.com>
2024-05-16 19:20:53 +0300

    remove visibility provider

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,14 +1,11 @@
 import ReactDOM from "react-dom/client";
-import { VisibilityProvider } from "./providers/VisibilityProvider";
 import App from "./App";
 import "./index.css";
 // ui kit
 import { NextUIProvider } from "@nextui-org/react";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <VisibilityProvider>
-    <NextUIProvider>
-      <App />
-    </NextUIProvider>
-  </VisibilityProvider>
+  <NextUIProvider>
+    <App />
+  </NextUIProvider>
 );
